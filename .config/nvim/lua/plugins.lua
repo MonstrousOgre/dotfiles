ebfef07e3918c4e50dbac1642e3c7677a9168c25
MonstrousOgre <monstrousogre0813@gmail.com>
2022-01-03 00:27:35 +0530

    Reorganizing neovim keymap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,14 +1,47 @@
 local fn = vim.fn
+
+-- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-	vim.cmd("packadd packer.nvim")
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Only required if you have packer configured as `opt`
 vim.cmd([[packadd packer.nvim]])
 
-return require("packer").startup(function(use)
+return packer.startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
 
@@ -16,11 +49,11 @@ return require("packer").startup(function(use)
 
 	use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
-	--[[use {]]
-	--[['glepnir/galaxyline.nvim',]]
-	--[[branch = 'main',]]
-	--[[requires = {'kyazdani42/nvim-web-devicons', opt = true}]]
-	--[[}]]
+	--use {
+	--'glepnir/galaxyline.nvim',
+	--branch = 'main',
+	--requires = {'kyazdani42/nvim-web-devicons', opt = true}
+	--}
 
 	use({ "famiu/feline.nvim", requires = { "lewis6991/gitsigns.nvim", "kyazdani42/nvim-web-devicons" } })
 
@@ -62,4 +95,10 @@ return require("packer").startup(function(use)
 
 	use("tpope/vim-fugitive")
 	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,49 +1,47 @@
-vim.o.hidden = true
-vim.o.termguicolors = true
-vim.o.fileencoding = "utf-8"
-vim.o.ambiwidth = "single"
--- vim.o.pumheight = 10 -- Makes popup menu smaller
-vim.o.ruler = true
--- vim.o.cmdheight = 2 -- More space for displaying messages
--- vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object
-vim.o.mouse = "a"
-vim.o.splitbelow = true -- Horizontal splits will automatically be below
-vim.o.splitright = true -- Vertical splits will automatically be to the right
--- vim.o.t_Co = '256'
-vim.o.conceallevel = 0 -- So that I can see `` in markdown files
-vim.o.smarttab = true
-vim.o.laststatus = 2
-vim.o.background = "dark"
-vim.o.showtabline = 2
-vim.o.showmode = false
-vim.o.backup = false
-vim.o.writebackup = false
--- vim.o.updatetime = 300
-vim.o.timeoutlen = 500
--- vim.o.timeoutlen = 300
-vim.o.clipboard = "unnamedplus"
-vim.o.shortmess = vim.o.shortmess .. "c"
--- vim.o.guicursor = ''
+local options = {
+	backup = false, -- creates a backup file
+	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+	--cmdheight = 2, -- more space in the neovim command line for displaying messages
+	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+	conceallevel = 0, -- so that `` is visible in markdown files
+	fileencoding = "utf-8", -- the encoding written to a file
+	hlsearch = true, -- highlight all matches on previous search pattern
+	ignorecase = true, -- ignore case in search patterns
+	mouse = "a", -- allow the mouse to be used in neovim
+	--pumheight = 10,                          -- pop up menu height
+	showmode = false, -- we don't need to see things like -- INSERT -- anymore
+	showtabline = 2, -- always show tabs
+	smartcase = true, -- smart case
+	smartindent = true, -- make indenting smarter again
+	splitbelow = true, -- force all horizontal splits to go below current window
+	splitright = true, -- force all vertical splits to go to the right of current window
+	swapfile = false, -- creates a swapfile
+	termguicolors = true, -- set term gui colors (most terminals support this)
+	timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
+	undofile = true, -- enable persistent undo
+	--updatetime = 300, -- faster completion (4000ms default)
+	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	expandtab = true, -- convert tabs to spaces
+	shiftwidth = 2, -- the number of spaces inserted for each indentation
+	tabstop = 2, -- insert 2 spaces for a tab
+	cursorline = true, -- highlight the current line
+	number = true, -- set numbered lines
+	relativenumber = true, -- set relative numbered lines
+	numberwidth = 4, -- set number column width to 2 {default 4}
+	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+	wrap = false, -- display lines as one long line
+	scrolloff = 8, -- is one of my fav
+	sidescrolloff = 8,
+	guifont = "monospace:h17", -- the font used in graphical neovim applications
+	colorcolumn = "99999",
+}
 
-vim.wo.wrap = false
-vim.o.number = true
-vim.wo.number = true
-vim.o.relativenumber = true
-vim.wo.relativenumber = true
-vim.wo.cursorline = true
+vim.opt.shortmess:append("c")
 
-vim.o.tabstop = 2
-vim.bo.tabstop = 2
-vim.o.shiftwidth = 2
-vim.bo.shiftwidth = 2
-vim.o.expandtab = true -- Converts tabs to spaces
-vim.bo.expandtab = true -- Converts tabs to spaces
-vim.o.smartindent = true
-vim.bo.smartindent = true
-vim.o.autoindent = true
-vim.bo.autoindent = true
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
 
-vim.wo.colorcolumn = "99999"
-
--- vim.g.mapleader = ' '
--- vim.b.mapleader = ' '
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd([[set iskeyword+=-]])
+vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
