ce7dbea093092fd7cf83bd1ec4c3b942d5be93f1
MonstrousOgre <monstrousogre0813@gmail.com>
2023-04-10 07:26:04 +0530

    adding wallpapers

--- .config/nvim/lua/config/feline.lua
+++ .config/nvim/lua/config/feline.lua
@@ -1,8 +1,8 @@
 --local components = require('feline.presets')["default"]
 
 local components = {
-	active = {},
-	inactive = {},
+  active = {},
+  inactive = {},
 }
 
 table.insert(components.active, {})
@@ -10,92 +10,92 @@ table.insert(components.active, {})
 table.insert(components.active, {})
 
 table.insert(components.active[1], {
-	provider = "vi_mode",
-	hl = function()
-		return {
-			name = require("feline.providers.vi_mode").get_mode_highlight_name(),
-			fg = require("feline.providers.vi_mode").get_mode_color(),
-			style = "bold",
-		}
-	end,
-	left_sep = " ",
-	right_sep = " ",
-	-- uncomment the next line to disable icons for this component and use the mode name instead
-	-- icon = ''
+  provider = "vi_mode",
+  hl = function()
+    return {
+      name = require("feline.providers.vi_mode").get_mode_highlight_name(),
+      fg = require("feline.providers.vi_mode").get_mode_color(),
+      style = "bold",
+    }
+  end,
+  left_sep = " ",
+  right_sep = " ",
+  -- uncomment the next line to disable icons for this component and use the mode name instead
+  -- icon = ''
 })
 
 table.insert(components.active[1], {
-	provider = "git_branch",
-	left_sep = " ",
-	right_sep = " ",
-	hl = {
-		fg = "yellow",
-	},
+  provider = "git_branch",
+  left_sep = " ",
+  right_sep = " ",
+  hl = {
+    fg = "yellow",
+  },
 })
 
 table.insert(components.active[1], {
-	provider = "git_diff_added",
-	right_sep = " ",
-	hl = {
-		fg = "green",
-	},
+  provider = "git_diff_added",
+  right_sep = " ",
+  hl = {
+    fg = "green",
+  },
 })
 
 table.insert(components.active[1], {
-	provider = "git_diff_changed",
-	right_sep = " ",
-	hl = {
-		fg = "orange",
-	},
+  provider = "git_diff_changed",
+  right_sep = " ",
+  hl = {
+    fg = "orange",
+  },
 })
 
 table.insert(components.active[1], {
-	provider = "git_diff_removed",
-	right_sep = " ",
-	hl = {
-		fg = "red",
-	},
+  provider = "git_diff_removed",
+  right_sep = " ",
+  hl = {
+    fg = "red",
+  },
 })
 
 table.insert(components.active[2], {
-	provider = "file_info",
-	hl = {
-		style = "bold",
-	},
+  provider = "file_info",
+  hl = {
+    style = "bold",
+  },
 })
 
 table.insert(components.active[3], {
-	provider = "diagnostic_info",
-	right_sep = " ",
-	hl = "DiagnosticSignInfo",
-	icon = " ",
+  provider = "diagnostic_info",
+  right_sep = " ",
+  hl = "DiagnosticSignInfo",
+  icon = " ",
 })
 
 table.insert(components.active[3], {
-	provider = "diagnostic_hints",
-	right_sep = " ",
-	hl = "DiagnosticSignHint",
-	icon = " ",
+  provider = "diagnostic_hints",
+  right_sep = " ",
+  hl = "DiagnosticSignHint",
+  icon = " ",
 })
 
 table.insert(components.active[3], {
-	provider = "diagnostic_warnings",
-	right_sep = " ",
-	hl = "DiagnosticSignWarn",
-	icon = " ",
+  provider = "diagnostic_warnings",
+  right_sep = " ",
+  hl = "DiagnosticSignWarn",
+  icon = " ",
 })
 
 table.insert(components.active[3], {
-	provider = "diagnostic_errors",
-	right_sep = " ",
-	hl = "DiagnosticSignError",
-	icon = " ",
+  provider = "diagnostic_errors",
+  right_sep = " ",
+  hl = "DiagnosticSignError",
+  icon = " ",
 })
 
 table.insert(components.active[3], {
-	provider = "lsp_client_names",
-	left_sep = " ",
-	right_sep = " ",
+  provider = "lsp_client_names",
+  left_sep = " ",
+  right_sep = " ",
 })
 
 --table.insert(components.active[3], {
@@ -104,20 +104,20 @@ table.insert(components.active[3], {
 --})
 
 table.insert(components.active[3], {
-	provider = "file_type",
-	right_sep = " ",
-	hl = {
-		fg = "skyblue",
-		style = "bold",
-	},
+  provider = "file_type",
+  right_sep = " ",
+  hl = {
+    fg = "skyblue",
+    style = "bold",
+  },
 })
 
 table.insert(components.active[3], {
-	provider = "position",
-	right_sep = " ",
-	hl = {
-		fg = "yellow",
-	},
+  provider = "position",
+  right_sep = " ",
+  hl = {
+    fg = "yellow",
+  },
 })
 
 --table.insert(components.active[3], {
@@ -130,8 +130,8 @@ table.insert(components.active[3], {
 --})
 
 require("feline").setup({
-	theme = {
-		bg = "NONE",
-	},
-	components = components,
+  theme = {
+    bg = "NONE",
+  },
+  components = components,
 })
--- .config/nvim/lua/config/lsp/lsp_auto_cmd.lua
+++ .config/nvim/lua/config/lsp/lsp_auto_cmd.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_exec("autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)", false)
+vim.api.nvim_exec("autocmd BufWritePre * lua vim.lsp.buf.format { async = false }", false)
 --[[
    [vim.api.nvim_exec(
 	 [  "autocmd CursorHold,CursorHoldI * lua require('config.lsp.code_action_utils').code_action_listener()",
