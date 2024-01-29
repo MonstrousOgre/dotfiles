b4dbfce7ee575c24d421948e0523e1c42845cd96
MonstrousOgre <monstrousogre0813@gmail.com>
2024-01-29 19:34:48 +0530

    adding relative and absolute line numbers

--- .config/nvim/lua/config/statuscol.lua
+++ .config/nvim/lua/config/statuscol.lua
@@ -4,9 +4,11 @@ require("statuscol").setup(
   {
     relculright = true,
     segments = {
-      { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
-      { text = { "%s" },                  click = "v:lua.ScSa" },
-      { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" }
+      { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+      { text = { "%s", " " },             click = "v:lua.ScSa" },
+      -- { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+      { text = { "%l", " " },             click = "v:lua.ScLa" },
+      { text = { "%r", " " },             click = "v:lua.ScLa" },
     }
   }
 )
