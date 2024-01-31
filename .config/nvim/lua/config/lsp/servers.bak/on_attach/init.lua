d13296d5167a27c3eaa40bbe4a49e5ff1e88da28
MonstrousOgre <monstrousogre0813@gmail.com>
2024-01-31 13:07:48 +0530

    removing old lsp server configurations

--- .config/nvim/lua/config/lsp/servers.bak/on_attach/init.lua
+++
@@ -1,5 +0,0 @@
-local keybindings = require("config.lsp.servers.on_attach.keybindings")
-
-return function(client, bufnr)
-	keybindings(client, bufnr)
-end
--- .config/nvim/lua/config/lsp/servers/init.lua
+++ .config/nvim/lua/config/lsp/servers/init.lua
@@ -11,21 +11,21 @@ mason_lspconfig.setup_handlers {
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
   function(server_name) -- default handler (optional)
-    if server_name == "html" or server_name == "cssls" or server_name == "tsserver" or server_name == "jsonls" or server_name == "yamlls" then
-      lspconfig[server_name].setup { on_attach = function(client, bufnr)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-        capabilities.textDocument.foldingRange = {
-          dynamicRegistration = false,
-          lineFoldingOnly = true
-        }
-        on_attach(client, bufnr)
-      end,
-        capabilities = capabilities
-      }
-    else
-      lspconfig[server_name].setup { on_attach = on_attach, capabilities = capabilities }
-    end
+    -- if server_name == "html" or server_name == "cssls" or server_name == "tsserver" or server_name == "jsonls" or server_name == "yamlls" then
+    --   lspconfig[server_name].setup { on_attach = function(client, bufnr)
+    --     client.server_capabilities.documentFormattingProvider = false
+    --     client.server_capabilities.documentRangeFormattingProvider = false
+    --     capabilities.textDocument.foldingRange = {
+    --       dynamicRegistration = false,
+    --       lineFoldingOnly = true
+    --     }
+    --     on_attach(client, bufnr)
+    --   end,
+    --     capabilities = capabilities
+    --   }
+    -- else
+    lspconfig[server_name].setup { on_attach = on_attach, capabilities = capabilities }
+    -- end
   end,
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
