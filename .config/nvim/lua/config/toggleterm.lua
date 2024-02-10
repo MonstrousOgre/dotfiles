62c3fb48978d35be94e1635e37d497ab924f375a
MonstrousOgre <monstrousogre0813@gmail.com>
2024-02-10 10:43:14 +0530

    adding nvidia stuff back to hyprland config

--- .config/nvim/lua/config/toggleterm.lua
+++ .config/nvim/lua/config/toggleterm.lua
@@ -1,5 +1,6 @@
 require("toggleterm").setup {
-  open_mapping = [[<F1>]],
+  -- open_mapping = [[<F1>]],
+  open_mapping = [[<C-t>]],
   direction = 'float',
   float_opts = {
     border = 'rounded',
