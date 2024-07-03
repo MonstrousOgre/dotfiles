f6f4bc6e6f9da1d70d0039d486a7bcfc811fa1c0
MonstrousOgre <monstrousogre0813@gmail.com>
2024-07-03 19:25:26 +0530

    gtklock stuff

--- .config/nvim/lua/config/floaterm.lua
+++ .config/nvim/lua/config/floaterm.lua
@@ -18,8 +18,8 @@ set_keymap("t", "<F4>", "<C-\\><C-n>:FloatermNew<CR>", { noremap = true, silent 
 -- Floaterm
 vim.g.floaterm_gitcommit = "floaterm"
 vim.g.floaterm_autoinsert = 1
--- vim.g.floaterm_width=0.8
--- vim.g.floaterm_height=0.8
+vim.g.floaterm_width = 0.7
+vim.g.floaterm_height = 0.7
 vim.g.floaterm_wintitle = 1
 vim.g.floaterm_title = "terminal: $1/$2"
 vim.g.floaterm_autoclose = 2
