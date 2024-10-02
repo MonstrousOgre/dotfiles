83f9a10f3ac8c5d4ab57e6c9f9e630a32e3e167e
MonstrousOgre <monstrousogre0813@gmail.com>
2024-10-02 11:58:28 +0530

    switching to new diagnostics api

--- .config/nvim/lua/config/lsp/diagnostics/init.lua
+++ .config/nvim/lua/config/lsp/diagnostics/init.lua
@@ -1,8 +1,22 @@
-require("config.lsp.diagnostics.signs")
+-- require("config.lsp.diagnostics.signs")
 
 vim.diagnostic.config({
   virtual_text = { severity = { min = vim.diagnostic.severity.ERROR } },
-  signs = true,
+  -- signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+      [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+      [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+    },
+  },
   underline = true,
   update_in_insert = false,
   severity_sort = false,
