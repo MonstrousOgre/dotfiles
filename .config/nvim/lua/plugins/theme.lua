d43da3223371aceba7fcde66e4b5030c7abba8e7
MonstrousOgre <monstrousogre0813@gmail.com>
2026-07-31 01:53:05 +0530

    changing nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -23,19 +23,45 @@
 --   end,
 -- }
 
+-- return {
+--   "rijulpaul/nightblossom.nvim",
+--   name = "nightblossom",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("nightblossom").setup({
+--       variant = "sakura",  -- "spring", "sakura", "pastel"
+--       transparent = true,  -- Enable background transparency
+--       integrations = {
+--         treesitter = true, -- TreeSitter highlighting
+--       }
+--     })
+--     vim.cmd.colorscheme("nightblossom-sakura")
+--   end,
+-- }
+
 return {
-  "rijulpaul/nightblossom.nvim",
-  name = "nightblossom",
+  "polirritmico/monokai-nightasty.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    require("nightblossom").setup({
-      variant = "sakura",  -- "spring", "sakura", "pastel"
-      transparent = true,  -- Enable background transparency
-      integrations = {
-        treesitter = true, -- TreeSitter highlighting
-      }
-    })
-    vim.cmd.colorscheme("nightblossom-sakura")
-  end,
+  config = function(
+  )
+    vim.opt.background = "dark" -- default to dark or light style
+
+    local opts = {
+      dark_style_background = "transparent",  -- default, dark, transparent, #RRGGBB
+      light_style_background = "transparent", -- default, dark, transparent, #RRGGBB
+      hl_styles = {
+        -- Background styles for floating windows and sidebars (panels):
+        floats = "transparent",   -- default, dark, transparent
+        sidebars = "transparent", -- default, dark, transparent
+      },
+    }
+    require("monokai-nightasty").setup(opts) -- ...and then load the theme:
+    require("monokai-nightasty").load()
+
+    -- As an alternative, pass the options directly into load and it will run setup
+    -- the first time it is executed:
+    require("monokai-nightasty").load(opts)
+  end
 }
