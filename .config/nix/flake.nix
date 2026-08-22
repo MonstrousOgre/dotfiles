cb81b18056f40806f77360e871108598dc78b68b
MonstrousOgre <monstrousogre0813@gmail.com>
2026-08-22 19:57:32 +0530

    Adding nix-darwin flake to dotfiles

---
+++ .config/nix/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+  }: let
+    configuration = {pkgs, ...}: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        vim
+        lazygit
+      ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      programs.zsh.enable = true;
+
+      users.users.ogre.shell = pkgs.zsh;
+    };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Kashs-MacBook-Air
+    darwinConfigurations."Kashs-MacBook-Air" = nix-darwin.lib.darwinSystem {
+      modules = [configuration];
+    };
+  };
+}
