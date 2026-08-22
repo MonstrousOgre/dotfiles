3b46ec013b832ee65f828149418cf8eab0aaf420
MonstrousOgre <monstrousogre0813@gmail.com>
2026-08-22 19:59:43 +0530

    cyberdream

--- .config/wezterm/appearance.lua
+++ .config/wezterm/appearance.lua
@@ -7,7 +7,7 @@ function M.apply_to_config(config)
 	config.colors = require("cyberdream")
 	config.window_background_opacity = 1.0
 
-	config.use_fancy_tab_bar = false
+	-- config.use_fancy_tab_bar = false
 
 	config.window_frame = {
 		-- active_titlebar_bg = "#1e1e1e",
@@ -21,7 +21,7 @@ function M.apply_to_config(config)
 
 	if wezterm.target_triple:find("linux") then
 		-- background = "#1d212f"
-		background = "#31313A"
+		-- background = "#31313A"
 		config.window_background_opacity = 0.8
 		-- config.wayland_window_background_blur = true
 		config.kde_window_background_blur = true
